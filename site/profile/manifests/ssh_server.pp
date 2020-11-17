f89ffd3a261d4c1822ac25637f0a18c96e927e05
TRS1080 <69827760+TRS1080@users.noreply.github.com>
2020-11-16 18:25:22 -0600

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCrROCM/zH8VR9RJutx4/QgsU1mn668QXLUmGoA08ExJQk2aB1KoJ8ZayHlG+QtSTgCjA/FaChq1jy/fFGQo0I2yZ88Sp8TtA8PhrR8X8GJLeqKuhGEpUtErI95W7vN8ma51rNka7vl4Z7MyGnslf6jYk1l+V7vf/4Oesk1/N3d9ea8hN3PQ+1sM1te8K4MtwfrvunMv7krWE2rdutnAe/8XVgUAlLSU0w9x1TAwjwNdjYOGn2nSWSUVayUhYrc04q42Oh70Sk9QVNHM7wsAR4RULYYMc7XD280/LiDNXf3IrGcQ+QjAxbDKzLkn0XSsLva5uZbksx5fiiL1sGDkQb9',
 	}  
 }
